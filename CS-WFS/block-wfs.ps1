e7c95115f143d03f97ecd8f6b513210b141bfbf4
sebastiensokoloff <sebastien.sokoloff@prolion.com>
2023-05-03 15:35:53 +0200

    Add Help comments

--- CS-WFS/block-wfs.ps1
+++ CS-WFS/block-wfs.ps1
@@ -5,7 +5,53 @@ param (
     [switch]$sync
 
 )
-#CS minimum version 3.0.17P2
+    <#
+        .SYNOPSIS
+        Blocks user access to smb file share on local server based on blocked users in CryptoSpike Application. CryptoSpike 3.0.17P1 or newer required.
+        Requires Powershell 7.
+
+        .DESCRIPTION
+        Blocks user access to smb file share on local server based on blocked users in CryptoSpike Application.
+        Should be ran on a schedule in order to update blocked users on the local SMB shares. Administrative shares are not modified.
+        Use of the $Sync parameter will remove all explicit blocks on protected shares not linked to a blocked user in CryptoSpike.
+        CryptoSpike 3.0.17P1 or newer required. Requires Powershell 7.
+
+        .PARAMETER ip
+        Specifies the ip of the CryptoSpike Leader Machine
+
+        .PARAMETER username
+        Specifies the username used to query the CryptoSpike Leader Machine. The user must be able to read blocked users.
+
+        .PARAMETER password
+        Specifies the password used to query the CryptoSpike Leader Machine
+
+        .PARAMETER sync
+        Use script in sync mode, will remove block permissions no longer needed.
+
+        .INPUTS
+        None. You cannot pipe objects to block-wfs.ps1.
+
+        .OUTPUTS
+        None.
+
+        .EXAMPLE
+        PS> .\block-wfs.ps1 -ip 1.1.1.1 -username "readonly" -password "string" 
+        Will query CryptoSpike for all Windows blocked users and block them on the local Windows File Server.
+
+        .EXAMPLE
+        PS> .\block-wfs.ps1 -ip 1.1.1.1 -username "readonly" -password "string" -sync
+        Will query CryptoSpike for all Windows blocked users and block them on the local Windows File Server.
+        Will also remove Unblocked users from the server.
+        
+        .LINK
+        Online version: https://github.com/ProLion-Engineering/CS-Powershell-Lib/tree/main/CS-WFS
+
+    #>
+
+#Requires -PSEdition Core
+#Requires -Version 7.0
+#Requires -RunAsAdministrator
+
 ###############
 #  Functions  #
 ###############
